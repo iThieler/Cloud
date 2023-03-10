36b70f28179bc06233efc2e37b1d3ee9d5fc45d1
iThieler <39486895+iThieler@users.noreply.github.com>
2023-03-10 08:37:25 +0100

    Update docker.sh

--- install/docker.sh
+++ install/docker.sh
@@ -1,1 +1,22 @@
 
+# System preparation
+apt-get update
+apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+# Docker GPG Key
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+# Docker Repository
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+exit 0
