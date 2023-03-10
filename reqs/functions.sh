677b586a587ab753c6553a22e4c8113ee9290dea
iThieler <39486895+iThieler@users.noreply.github.com>
2023-03-10 15:10:55 +0100

    Update functions.sh

--- reqs/functions.sh
+++ reqs/functions.sh
@@ -37,27 +37,41 @@ function generateAPIKey() {
   done 
 }
 
-function updateAndUpgrade() {
+function UpdateAndUpgrade() {
   # This function performs a complete system update of the server
   # ----------
   # Call with: updateAndUpgrade
   # ----------
   {
-    echo -e "XXX\n12\nSystemupdate wird ausgeführt ...\nXXX"
-    if ! apt-get update 2>&1 >/dev/null; then false; fi
-    echo -e "XXX\n25\nSystemupdate wird ausgeführt ...\nXXX"
-    if ! apt-get upgrade -y 2>&1 >/dev/null; then false; fi
-    echo -e "XXX\n47\nSystemupdate wird ausgeführt ...\nXXX"
-    if ! apt-get dist-upgrade -y 2>&1 >/dev/null; then false; fi
-    echo -e "XXX\n64\nSystemupdate wird ausgeführt ...\nXXX"
-    if ! apt-get autoremove -y 2>&1 >/dev/null; then false; fi
-    echo -e "XXX\n79\nSystemupdate wird ausgeführt ...\nXXX"
-    if ! pveam update 2>&1 >/dev/null; then false; fi
-    echo -e "XXX\n98\nSystemupdate wird ausgeführt ...\nXXX"
-  } | whiptail --gauge --backtitle "© 2021 - iThieler's Proxmox Script collection" --title " SYSTEMVORBEREITUNG " "Dein HomeServer wird auf Systemupdates geprüft ..." 10 80 0
-
-  # install DarkMode
-  if ! bash <(curl -s https://raw.githubusercontent.com/Weilbyte/PVEDiscordDark/master/PVEDiscordDark.sh) install 2>&1 >/dev/null; then false; fi
+    echo -e "XXX\n12\n${lang_updateupgrade_execmessage}\nXXX"
+    if ! apt update 2>&1 >/dev/null; then false; fi
+    echo -e "XXX\n35\n${lang_updateupgrade_execmessage}\nXXX"
+    if ! apt upgrade -y 2>&1 >/dev/null; then false; fi
+    echo -e "XXX\n51\n${lang_updateupgrade_execmessage}\nXXX"
+    if ! apt dist-upgrade -y 2>&1 >/dev/null; then false; fi
+    echo -e "XXX\n74\n${lang_updateupgrade_execmessage}\nXXX"
+    if ! apt autoremove -y 2>&1 >/dev/null; then false; fi
+    echo -e "XXX\n98\n${lang_updateupgrade_execmessage}\nXXX"
+  } | whiptail --gauge --backtitle "${lang_backtitle}" --title " ${lang_updateupgrade_title} " "${lang_updateupgrade_mainmessage}" 10 80 0
   
   return 0
 }
+
+function checkIP() {
+  # This function returns true if the given IP address is reachable, if not, you can check the IP address and change it if necessary.
+  # ----------
+  # Call with: checkIP "192.168.0.1"      !!! Note - 192.168.0.1 is the IP address to be checked in this example !!!
+  # use in an if-query: if checkIP "192.168.0.1"; then ipExist=true; else ipExist=false; fi
+  # use in an if-query: if checkIP "${IPVAR}"; then ipExist=true; else ipExist=false; fi
+  # ----------
+  function ping() { if ping -c 1 $1 &> /dev/null; then true; else false; fi }
+  
+  if [ -n $1 ]; then ip="$1"; else ip=""; fi
+  
+  while ! ping ${ip}; do
+    ip=$(whip_alert_inputbox_cancel "${lang_okbutton}" "${lang_cancelbutton}" "${lang_checkip_title}" "${lang_checkip_mainmessage}" "${ip}")
+    RET=$?
+    if [ $RET -eq 1 ]; then return 1; fi  # Check if User selected cancel
+  done
+}
+
